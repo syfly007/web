269ad02eacccd6f66088f9a9043a57c65fa40844
syfly007@163.com <sunyufei@sunyufeis-MacBook-Pro.local>
2016-07-08 07:36:45 +0800

    add

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,7 +1,9 @@
 from __future__ import unicode_literals
 
-from django.db import models
+import datetime
 
+from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -11,8 +13,8 @@ class Question(models.Model):
         return self.question_text
         
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
