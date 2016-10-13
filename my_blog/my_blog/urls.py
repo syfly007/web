6726b8e7664c85ef55b05d91617b0382a68d7f36
syfly007 <syfly007@163.com>
2016-10-13 09:56:11 +0800

    delete

--- my_blog/my_blog/urls.py
+++ my_blog/my_blog/urls.py
@@ -1,7 +1,7 @@
-"""mytest URL Configuration
+"""my_blog URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
+from article import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home,name="home"),
+    url(r'^home/$', views.home,name="home"),
+    url(r'^test/$', views.test,name="test"),
 ]
